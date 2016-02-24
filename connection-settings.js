52b1ce38ee7450d9a370d3458f6094d212b55956
Apoorv Umang <apoorvumang@gmail.com>
2016-02-24 19:55:18 +0530

    changed db conn settings

--- connection-settings.js
+++ connection-settings.js
@@ -5,7 +5,8 @@
 var mysql = require('mysql');
 
 module.exports.connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : '127.0.0.1',
+  port     : '3306',
   user     : 'adminSgjdugT',
   password : 'jYr9aHsfDyik',
   database : 'nodejs'
