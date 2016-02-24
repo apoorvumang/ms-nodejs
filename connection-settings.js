6cb78783a5d1f486c7f03aef4669b15c2947dbff
Apoorv Umang <apoorvumang@gmail.com>
2016-02-24 20:24:52 +0530

    changed db conn settings

--- connection-settings.js
+++ connection-settings.js
@@ -4,9 +4,12 @@
 
 var mysql = require('mysql');
 
+var mysql_host = process.env.OPENSHIFT_MYSQL_DB_HOST || '127.0.0.1';
+var mysql_port = process.env.OPENSHIFT_MYSQL_DB_PORT || '3306';
+
 module.exports.connection = mysql.createConnection({
-  host     : '127.0.0.1',
-  port     : '3306',
+  host     : mysql_host,
+  port     : mysql_port,
   user     : 'adminSgjdugT',
   password : 'jYr9aHsfDyik',
   database : 'drmahima_com'
