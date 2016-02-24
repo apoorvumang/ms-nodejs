b17315e49aa2890ccd9637eb6367e4956a793647
Apoorv Umang <apoorvumang@gmail.com>
2016-02-24 21:00:09 +0530

    dunno what

--- connection-settings.js
+++ connection-settings.js
@@ -7,11 +7,18 @@ var mysql = require('mysql');
 var mysql_host = process.env.OPENSHIFT_MYSQL_DB_HOST || '127.0.0.1';
 var mysql_port = process.env.OPENSHIFT_MYSQL_DB_PORT || '3306';
 
+// module.exports.connection = mysql.createConnection({
+//   host     : mysql_host,
+//   port     : mysql_port,
+//   user     : 'adminSgjdugT',
+//   password : 'jYr9aHsfDyik',
+//   database : 'drmahima_com'
+// });
+
 module.exports.connection = mysql.createConnection({
-  host     : mysql_host,
-  port     : mysql_port,
-  user     : 'adminSgjdugT',
-  password : 'jYr9aHsfDyik',
+  host     : 'localhost',
+  user     : 'root',
+  password : 'pass',
   database : 'drmahima_com'
 });
 
